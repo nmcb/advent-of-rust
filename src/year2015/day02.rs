5ab2db661b2c76f888688aa5a9aa1934b70163f8
nmcb <nmcb@xs4all.nl>
2025-06-30 13:43:52 +0200

    solved 2015 day 02

--- src/year2015/day02.rs
+++ src/year2015/day02.rs
@@ -1,18 +1,42 @@
-pub fn answer1(input: &str) -> i32 {
-    let mut result = 0; 
-    for c in input.chars() {
-        if c == '(' { result += 1 } else { result -= 1 }
-    }
-    result
+use std::num::ParseIntError;
+
+struct Present {
+    l: u32,
+    w: u32,
+    h: u32,
 }
 
-pub fn answer2(input: &str) -> i32 {
-    let mut counter = 0;
-    let mut result = 0;
-    for c in input.chars() {
-        counter += 1;
-        if c == '(' { result += 1 } else { result -= 1 }
-        if result == -1 { break }
+impl Present {
+    fn paper_required(&self) -> u32 {
+        3 * self.l * self.w + 2 * self.w * self.h + 2 * self.h * self.l
     }
-    counter
+    fn ribbon_required(&self) -> u32 {
+        2 * (self.l + self.w) + (self.l * self.w) * self.h
+    }
+}
+
+fn parse(input: &str) -> Vec<Present> {
+    let presents: Result<Vec<Present>, ParseIntError> = input.lines().map(|l| {
+            l.split('x')
+                .map(|f| f.parse::<u32>())
+                .collect::<Result<Vec<u32>, std::num::ParseIntError>>()
+                .map(|mut p| {
+                    p.sort_unstable();
+                    Present { l: p[0], w: p[1], h: p[2] }
+                })
+        })
+        .collect();
+    match presents {
+        Ok(result) => result,
+        Err(msg) => panic!("error: {}", msg)
+    }
+
+}
+
+pub fn answer1(input: &str) -> u32 {
+    parse(input).iter().map(|p| p.paper_required()).sum()
+}
+
+pub fn answer2(input: &str) -> u32 {
+    parse(input).iter().map(|p| p.ribbon_required()).sum()
 }
