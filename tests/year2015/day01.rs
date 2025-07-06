cc3e06bcc7f57f09c764d2d953172848b58db74b
nmcb <nmcb@xs4all.nl>
2025-07-06 13:49:50 +0200

    alligned let statements

--- tests/year2015/day01.rs
+++ tests/year2015/day01.rs
@@ -4,14 +4,14 @@ use crate::input;
 
 #[test]
 fn answer1_test() {
-    let input = parse(input("year2015", "day01").as_str());
+    let input  = parse(input("year2015", "day01").as_str());
     let answer = solve1(&input);
     assert_eq!(answer, 138);
 }
 
 #[test]
 fn answer2_test() {
-    let input = parse(input("year2015", "day01").as_str());
+    let input  = parse(input("year2015", "day01").as_str());
     let answer = solve2(&input);
     assert_eq!(answer, 1771);
 }
--- tests/year2015/day02.rs
+++ tests/year2015/day02.rs
@@ -3,14 +3,14 @@ use crate::input;
 
 #[test]
 fn answer1_test() {
-    let input = parse(input("year2015", "day02").as_str());
+    let input  = parse(input("year2015", "day02").as_str());
     let answer = solve1(&input);
     assert_eq!(answer, 1606483);
 }
 
 #[test]
 fn answer2_test() {
-    let input = parse(input("year2015", "day02").as_str());
+    let input  = parse(input("year2015", "day02").as_str());
     let answer = solve2(&input);
     assert_eq!(answer, 3842356);
 }
