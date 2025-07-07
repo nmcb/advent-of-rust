179d8f37b9cadea26a5f619468e6f2b0203c5543
nmcb <nmcb@xs4all.nl>
2025-07-07 09:36:11 +0200

    fixed 2015 day 04 test

--- tests/year2015/day04.rs
+++ tests/year2015/day04.rs
@@ -3,14 +3,16 @@ use crate::input;
 
 #[test]
 fn answer1_test() {
-    let input  = parse(input("year2015", "day04").as_str());
+    let data   = input("year2015", "day04");
+    let input  = parse(data.as_str());
     let answer = solve1(&input);
     assert_eq!(answer, 282749);
 }
 
 #[test]
 fn answer2_test() {
-    let input  = parse(input("year2015", "day04").as_str());
+    let data   = input("year2015", "day04");
+    let input  = parse(data.as_str());
     let answer = solve2(&input);
     assert_eq!(answer, 9962624);
 }
